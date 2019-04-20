fedb70782d1e80fed25de20476e836752e1441f6
Christophe Savard <christophe_savard@hotmail.ca>
2019-04-20 00:43:40 -0400

    Added files

---
+++ ConfigFieldAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/* RealChute 2 is distributed under CC BY-NC-ND 4.0 INTL (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+ * The NoDerivs clause may be lifted and distribution of derivative works may be attributed to individuals on a case by case basis.
+ * If you wish to obtain such rights, please contact the owner (Christophe Savard) directly through a private channel. */
+
+namespace RealChute.ConfigLoader
+{
+    /// <summary>
+    /// Attribute used to mark fields and properties that can be loaded via ConfigObject
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public sealed class ConfigFieldAttribute : Attribute
+    {
+        #region Properties
+        /// <summary>
+        /// If this config field is mandatory cor the loading process to correctly proceed
+        /// </summary>
+        public bool Mandatory { get; set; } = false;
+        /// <summary>
+        /// The name of the field in the config, if left blank, the name of the code field is used
+        /// </summary>
+        public string CustomName { get; set; } = null;
+        #endregion
+    }
+}
---
+++ MissingConfigFieldException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/* RealChute 2 is distributed under CC BY-NC-ND 4.0 INTL (https://creativecommons.org/licenses/by-nc-nd/4.0/)
+ * The NoDerivs clause may be lifted and distribution of derivative works may be attributed to individuals on a case by case basis.
+ * If you wish to obtain such rights, please contact the owner (Christophe Savard) directly through a private channel. */
+
+namespace RealChute.ConfigLoader
+{
+    /// <summary>
+    /// Exception thrown when a config field is expected but not found
+    /// </summary>
+    public class MissingConfigFieldException : Exception
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates a new MissingConfigException
+        /// </summary>
+        public MissingConfigFieldException() { }
+
+        /// <summary>
+        /// Creates a new MissingConfigFieldException with the given message
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public MissingConfigFieldException(string message) : base(message) { }
+        #endregion
+    }
+}
