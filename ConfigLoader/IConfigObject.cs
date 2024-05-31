83154f576f1c1cb0ab22400f894a6d5ce0ba9d89
Christophe Savard <christophe_savard@hotmail.ca>
2024-05-31 01:03:25 -0400

    add IConfigObject

---
+++ ConfigLoader/IConfigObject.cs
@@ -0,0 +1,18 @@
+﻿/* ConfigLoader is distributed under CC BY-NC-SA 4.0 INTL (https://creativecommons.org/licenses/by-nc-sa/4.0/)
+ * You are free to redistribute, share, adapt, etc. as long as the original author stupid_chris (Christophe Savard) is properly,
+ * clearly, and explicitly credited, that you do not use this material to a commercial use, and that you share-alike. */
+
+namespace ConfigLoader;
+
+/// <summary>
+/// Autoloading Config Object interface
+/// </summary>
+public interface IConfigObject
+{
+    #region Properties
+    /// <summary>
+    /// Name of the ConfigNode to save/load to
+    /// </summary>
+    string Name { get; }
+    #endregion
+}
